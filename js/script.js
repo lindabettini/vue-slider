3bfa1c9b507ecac1b45d14a710a9b79d476c8da4
lindabettini <linda.bettini@hotmail.it>
2022-01-27 22:36:31 +0100

    to be fixed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,56 @@
-console.log('JS OK');
+console.log('VUE OK', Vue);
+
+Vue.config.devtools = true; 
+
+const app = new Vue ({
+    el: '#root',
+    data: {
+        autoplay: undefined, 
+        isAutoplay: false,
+        autoplayButton: {
+            className: 'btn-primary', 
+            text: 'Play'
+        },
+        currentIndex: 0,    
+        images: [
+        'images/image1.jpg',
+        'images/image2.jpg',
+        'images/image3.jpg',
+        'images/image4.jpg',
+        ]
+    },
+    methods: {
+        isActive(index) {
+            return index === this.currentIndex;
+        }, 
+
+        setPic(index) {
+            this.currentIndex = index; 
+        }, 
+
+        prevPic() {
+            if (this.currentIndex === 0) this.currentIndex = this.images.length - 1; 
+            else this.currentIndex--;
+        },
+
+        nextPic() {
+            if (this.currentIndex === this.images.length - 1) this.currentIndex = 0; 
+            else this.currentIndex++;
+        },
+
+        // & AUTOPLAY
+        startAutoplay() {
+            this.autoplay = setInterval(this.nextPic, 3000);
+            this.isAutoplay = true;
+        }, 
+
+        stopAutoplay() {
+            clearInterval(this.autoplay);
+            this.isAutoplay = false;
+        },
+
+        toggleAutoplay(){
+            this.isAutoplay ? this.stopAutoplay() : this.startAutoplay();
+        }
+    }
+})
